ee5dda876faf188bf806a4f9c6fda0f0c0c1eeee
Sam Peck <fullstacksam@protonmail.com>
2023-06-13 17:36:54 -0400

    hey now hey now

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    if len(num_list)<1:
+        return([])
+    else:
+         returned_list =[ num * 1 for num in num_list if num % 2 ==0]
+         return returned_list
 
 def make_exclamation(sentence_list):
-    pass
+    if len(sentence_list) <1:
+        return([])
+    else:
+        returned_with_exclamtions =[sentance + "!" for sentance in sentence_list]
+        return returned_with_exclamtions
